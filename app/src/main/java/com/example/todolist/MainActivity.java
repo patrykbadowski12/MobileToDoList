d30ea2efd9fb683b45906d790cf9323560b74ac4
Patryk Badowski <patryk_badowski@o2.pl>
2020-01-15 21:57:26 +0100

    Add functionality to create tasks

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,6 +18,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Task> taskArray = new ArrayList<>();
+    TextView addTask;
+    DatePicker datePicker;
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +29,27 @@ public class MainActivity extends AppCompatActivity {
         taskArray.add(new Task("task 1", new Date(), false));
         taskArray.add(new Task("task 2", new Date(), true));
         taskArray.add(new Task("task 1", new Date(), false));
-        taskArray.add(new Task("task 2", new Date(), true));
-        taskArray.add(new Task("task 1", new Date(), false));
-        taskArray.add(new Task("task 2", new Date(), true));
-        taskArray.add(new Task("task 1", new Date(), false));
-        taskArray.add(new Task("task 2", new Date(), true));
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         TasksListAdapter adapter = new TasksListAdapter(this, taskArray);
         NoScrollListView itemsListView  = findViewById(R.id.task_list_view);
         itemsListView.setAdapter(adapter);
+    }
 
+    public void addTask(View view) {
+        addTask = findViewById(R.id.editText2);
+        datePicker = findViewById(R.id.simpleDatePicker);
+        int dayOfMonth = datePicker.getDayOfMonth() + 1;
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+        timePicker = findViewById(R.id.timePicker);
+        int minute = timePicker.getCurrentMinute();
+        Integer currentHour = timePicker.getCurrentHour();
+        Date date = new Date(year,month,dayOfMonth,currentHour,minute);
+
+        Task newTask = new Task(addTask.getText().toString(), date, false);
+        taskArray.add(newTask);
+        System.out.println(newTask);
+        System.out.println(taskArray);
     }
 }
--- app/src/main/java/com/example/todolist/Task.java
+++ app/src/main/java/com/example/todolist/Task.java
@@ -38,4 +38,13 @@ public class Task {
     public void setDone(boolean done) {
         this.done = done;
     }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "content='" + content + '\'' +
+                ", date=" + date +
+                ", done=" + done +
+                '}';
+    }
 }
